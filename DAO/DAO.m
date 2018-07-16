c799461fd650e92a9e2456b04270bfb449101279
saharsamr <sahar.rajabi76@gmail.com>
2018-07-16 13:50:40 +0430

    DAO basics.

---
+++ DAO/DAO.m
@@ -0,0 +1,11 @@
+classdef DAO
+    properties (Access = private)
+        dataFolder
+        dataList
+        startTime
+        endTime
+        barSamplingFreq
+        closeFigFlag %TODO: ask to know! :))
+        timeData
+    end
+end
