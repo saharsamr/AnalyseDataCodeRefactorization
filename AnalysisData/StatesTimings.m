e4106d1d9aca5135699fb30b9c05b6525fbe9516
saharsamr <sahar.rajabi76@gmail.com>
2018-07-16 12:29:41 +0430

    States timings class basics added.

---
+++ AnalysisData/StatesTimings.m
@@ -0,0 +1,11 @@
+classdef StatesTimings
+    properties (Access = private)
+        Fixation
+        Cue
+        Stimulus
+        KeepTrigger
+        ReleaseTrigger
+        Trigger
+        ChangedStimulus
+    end
+end
