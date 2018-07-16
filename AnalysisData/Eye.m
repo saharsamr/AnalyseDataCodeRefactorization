824bd7089b2266f51b62a434778bc662330cc677
saharsamr <sahar.rajabi76@gmail.com>
2018-07-16 12:21:10 +0430

    Eye base class added.

---
+++ AnalysisData/Eye.m
@@ -0,0 +1,10 @@
+classdef Eye
+    properties (Access = private)
+        Time = [];
+        Pa = []; %TODO: ask to find a better name.
+        PosX = [];
+        PosY = [];
+        ErrorVal
+        SaccadeTime = [];
+    end
+end
