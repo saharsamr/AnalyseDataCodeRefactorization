399b87b8981a9b046c5b70734e1d43a3baa5b50d
saharsamr <sahar.rajabi76@gmail.com>
2018-07-16 12:26:29 +0430

    TTWs class basics added.

---
+++ AnalysisData/TTWs.m
@@ -0,0 +1,13 @@
+% TODO: All possible TTWs are set?
+
+classdef TTW
+    properties (Access = private)
+        RealFixationTTW
+        RealCueTTW
+        RealNothingTTW
+        RealStimulusTTW
+        RealReleaseWaitTTW
+        RealChangeStimulusTTW
+        RealRewardTTW
+    end
+end
