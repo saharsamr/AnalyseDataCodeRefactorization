638c3a727e1f90cd88f7cd01b6c316bf27574a61
saharsamr <sahar.rajabi76@gmail.com>
2018-07-18 09:32:40 +0430

    replace .txt config file with a static matlab class.

---
+++ CONFIG/Config.m
@@ -0,0 +1,13 @@
+classdef Config
+    properties (Constant)
+        SUBJECT_NAME = 'Chiko'
+        TASK_NAME = 'Attention'
+        RESEARCHER_FIRST_NAME = 'Jafar'
+        RESEARCHER_LAST_NAME = ''
+        TIME_START = '2018-07-15 08:00:00'
+        TIME_END = '2018-07-15 18:30:00'
+        BAR_SAMPLING_FREQ = 2000
+        CLOSE_FIG_FLAG = 0
+        DATA_PATH = 'C:\Users\Sahar\Desktop\Refactored Codes\Data\EDF Data'
+    end
+end
