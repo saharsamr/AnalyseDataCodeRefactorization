37b209a1dcd37983ffcca367c81501fe4175841d
saharsamr <sahar.rajabi76@gmail.com>
2018-08-11 09:48:48 +0430

    nothing.

--- +CONFIG/Config.m
+++ +CONFIG/Config.m
@@ -4,8 +4,8 @@ classdef Config
         TASK_NAME = 'Hartley'
         RESEARCHER_FIRST_NAME = 'Jafar'
         RESEARCHER_LAST_NAME = ''
-        TIME_START = '2018-07-30 08:00:00'
-        TIME_END = '2018-07-30 18:30:00'
+        TIME_START = '2018-08-07 08:00:00'
+        TIME_END = '2018-08-07 18:30:00'
         BAR_SAMPLING_FREQ = 2000
         CLOSE_FIG_FLAG = 0
         EYELINK_DATA_PATH = 'E:\\IPM\\EyeLink_DataExtraction\\RefactoredCodes\\Data\\Hartley\\'
